ab9f5a113ce1d7c6c81ff83bb98393144c23af62
Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
2025-06-21 00:06:43 +0530

    chore:delete build outputs

--- dist/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_controller_1 = require("../controllers/users.controller");
-const catchAsync_1 = require("../utils/catchAsync");
-const router = (0, express_1.Router)();
-router.post("/", (0, catchAsync_1.catchAsync)(users_controller_1.createUser));
-exports.default = router;
